fbe41c2266209f9298a86cd4556d897f8bb5e1bd
mukesh644009 <er.muks1988@gmail.com>
2024-06-04 09:02:33 +0530

    added one more query

---
+++ src/leetcode/FirstScripts.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class FirstScripts {
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in); //System.in is a standard input stream.
+		System.out.print("Enter a string: ");
+		String str= sc.nextLine(); //reads string.
+		
+		String output = str.replaceAll(" ", "_").toLowerCase();
+		
+		DateFormat df = new SimpleDateFormat("dd_MM_yyyy");
+		String data = output + df.format(new Date());
+		
+		
+		//String finalString = output+ new Date().toString();
+		System.out.println(data);
+		
+	}
+
+}
--- src/notation/Test.java
+++
@@ -1,9 +0,0 @@
-package notation;
-
-public class Test {
-	
-	public static void main(String[] args) {
-		System.out.println("jai mata di..");
-	}
-
-}
